9eeee81a4b4f400ab1e5db7e9644426acce5e831
Silvia Trejo <silvia.trejo@fdmgroup.com>
2023-02-14 12:06:22 -0500

    Finished putting subclasses and deleted them. Eliminated tests that made sense with the old code structure but were redundant with the new code structure.

--- theMoneyExample/src/test/java/theMoneyExampleTest/MoneyTest.java
+++ theMoneyExample/src/test/java/theMoneyExampleTest/MoneyTest.java
@@ -21,9 +21,7 @@ public class MoneyTest {
 	@Test
 	public void testEquality() {
 		assertTrue(Money.dollar(5).equals(Money.dollar(5)));
-		assertFalse(Money.dollar(5).equals(Money.dollar(6)));
 		assertTrue(Money.franc(5).equals(Money.franc(5)));
-		assertFalse(Money.franc(5).equals(Money.franc(6)));
 		assertFalse(Money.franc(5).equals(Money.dollar(5)));
 		
 	}
@@ -42,9 +40,6 @@ public class MoneyTest {
 		assertEquals("CHF", Money.franc(1).currency());
 	}
 	
-	@Test
-	public void testDifferentClassEquality() {
-		assertFalse(new Money(10, "CHF").equals(new Franc(10, "CHF")));
-	}
+
 
 }
