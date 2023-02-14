a8f1d9d4d749df6c4a8b483c7f546e953731f961
Silvia Trejo <silvia.trejo@fdmgroup.com>
2023-02-13 23:56:33 -0500

    Chapter 1: Multi_Currency Money

--- theMoneyExample/src/main/java/theMoneyExample/Dollar.java
+++ theMoneyExample/src/main/java/theMoneyExample/Dollar.java
@@ -5,11 +5,11 @@ public class Dollar {
 	public int amount;
 
 	public Dollar(int amount) {
-		// TODO Auto-generated constructor stub
+		this.amount = amount;
 	}
 
 	public void times(int multiplier) {
-		// TODO Auto-generated method stub
+		amount *= multiplier;
 		
 	}
 
