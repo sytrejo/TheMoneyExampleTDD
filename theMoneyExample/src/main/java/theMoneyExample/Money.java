3e5c8d4d569062bb5bc37cabc2c7474390efcd60
Silvia Trejo <silvia.trejo@fdmgroup.com>
2023-02-14 10:37:06 -0500

    Refactoring by adding a factory method for franc in money superclass.

--- theMoneyExample/src/main/java/theMoneyExample/Money.java
+++ theMoneyExample/src/main/java/theMoneyExample/Money.java
@@ -15,5 +15,9 @@ public abstract class Money {
 	public static Money dollar(int amount) {
 		return new Dollar(amount);
 	}
+	
+	public static Money franc(int amount) {
+		return new Franc(amount);
+	}
 
 }
